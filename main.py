4273047283d2bb8e1bfdfe357f1bb71b9bd7a848
Aurélien Vauthier <vaua2803@usherbrooke.ca>
2019-11-24 16:50:34 -0500

    5th task now working

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import numpy as np
 from kmodes.kmodes import KModes    #https://github.com/nicodv/kmodes/
 from sklearn.cluster import KMeans  #https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
 
+pd.set_option('mode.chained_assignment', None)  # disable pandas' SettingWithCopy warning
 COL_NAMES = ["age", "workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation", "relationship",
-            "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "target"]
+             "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "target"]
 
 def importData():
 
@@ -44,7 +45,7 @@ def initStatsList(kmodes):
 
     # stat for each cluster = [trainCount, trainCount<=50K, trainCount>50K, clusterClass, trainingError, testCount, testCount<=50K, testCount>50K, testError]
     #                              0              1               2               3             4            5            6               7            8
-    stats = np.hstack((np.reshape(stats, (-1, 1)), np.zeros((kmodes.n_clusters, 8)))).tolist()
+    stats = np.hstack((np.reshape(stats, (-1, 1)), np.zeros((len(clusterIDs), 8)))).tolist()
     clustersStats = dict(zip(clusterIDs, stats))
 
     return clustersStats
@@ -73,46 +74,92 @@ def computeErrors(clustersStats, trainSize, testSize):
 
         if stats[3] == "<=50K":
             stats[4] = stats[2] / stats[0]
-            stats[8] = stats[7] / stats[5]
+            stats[8] = stats[7] / stats[5] if stats[5] > 0 else 0
         else:
             stats[4] = stats[1] / stats[0]
-            stats[8] = stats[6] / stats[5]
+            stats[8] = stats[6] / stats[5] if stats[5] > 0 else 0
 
-        print("Cluster stats : ", stats)
+        # print("Cluster stats : ", stats)
         globalTrainError += stats[4] * (stats[0] / trainSize)
         globalTestError += stats[8] * (stats[5] / testSize)
 
     return globalTrainError, globalTestError
 
-def main():
-    train, test = importData()
+def computeSats(kmodes, trainData, testData):
+    # testing
+    testData["prediction"] = kmodes.predict(testData.drop(columns="target"))
+
+    # compute statistics
+    clustersStats = initStatsList(kmodes)
+    countTrainingData(clustersStats, trainData, kmodes.labels_)
+    countTestingData(clustersStats, testData[["target", "prediction"]].values)
+    globalTrainError, globalTestError = computeErrors(clustersStats, trainData.shape[0], testData.shape[0])
+
+    # dropping predictions after use to not influence next iteration
+    testData.drop(columns="prediction", inplace=True)
+
+    return globalTrainError, globalTestError
+
+def findGoodK(trainData, testData):
+    bestK = None
+    bestError = float("inf")
 
     for k in np.unique(np.geomspace(2, 75, 20, dtype=int)):
-        print("--- Computing clustering with {} clusters ---\n".format(k))
+        print("--- Computing clustering with {} clusters ---".format(k))
 
         # training
-        kmodes = KModes(n_clusters=k, verbose=False, n_jobs=-1) #https://github.com/nicodv/kmodes/blob/master/kmodes/kmodes.py#L294
-        kmodes.fit(train.drop(columns="target"))
+        kmodes = KModes(n_clusters=k, n_jobs=-1) #https://github.com/nicodv/kmodes/blob/master/kmodes/kmodes.py#L294
+        kmodes.fit(trainData.drop(columns="target"))
 
-        # testing
-        test["prediction"] = kmodes.predict(test.drop(columns="target"))
-
-        # Print training statistics
-        print('Final training cost: {}'.format(kmodes.cost_))
-        print('Training iterations: {}'.format(kmodes.n_iter_))
-
-        # compute statistics
-        clustersStats = initStatsList(kmodes)
-        countTrainingData(clustersStats, train, kmodes.labels_)
-        countTestingData(clustersStats, test[["target", "prediction"]].values)
-
-        # dropping predictions after use to not influence next iteration
-        test.drop(columns="prediction", inplace=True)
-
-        globalTrainError, globalTestError = computeErrors(clustersStats, train.shape[0], test.shape[0])
+        # Compute stats
+        globalTrainError, globalTestError = computeSats(kmodes, trainData, testData)
 
         print("Global training error : {0:.2f} %".format(globalTrainError*100))
-        print("Global testing  error : {0:.2f} %".format(globalTestError*100))
+        print("Global testing  error : {0:.2f} %\n".format(globalTestError*100))
+
+        if globalTestError < bestError:
+            bestError = globalTestError
+            bestK = k
+
+    print("Best K found : {} with test error of {} %".format(bestK, bestError*100))
+    return bestK
+
+def greedyAlgorithm(k, currentVariables, trainData, testData):
+    unwantedVariables = currentVariables + ["capital-gain", "capital-loss", "hours-per-week", "target"]
+    variablesToTest = [var for var in COL_NAMES if var not in unwantedVariables]
+
+    # stop condition
+    if not variablesToTest:
+        return currentVariables
+
+    bestVariable = None
+    bestError = float("inf")
+    for variable in variablesToTest:
+        trainingSet = currentVariables + [variable]
+        testingSet = trainingSet + ["target"]
+
+        kmodes = KModes(n_clusters=k, n_jobs=-1)
+        kmodes.fit(trainData[trainingSet])
+
+        # Compute stats
+        globalTrainError, globalTestError = computeSats(kmodes, trainData, testData[testingSet])
+
+        if globalTestError < bestError:
+            bestError = globalTestError
+            bestVariable = variable
+
+    currentVariables.append(bestVariable)
+    print("Best set found : {} with test error of {} %".format(currentVariables, bestError*100))
+
+    return greedyAlgorithm(k, currentVariables, trainData, testData)
+
+def main():
+    trainData, testData = importData()
+
+    # k = findGoodK(trainData, testData)
+    k = 16
+
+    greedyAlgorithm(k, [], trainData, testData)
 
 if __name__ == '__main__':
     main()
